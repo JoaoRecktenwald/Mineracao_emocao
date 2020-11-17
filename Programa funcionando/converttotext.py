7455c8931aef67c98141c03c53b5da834032e4aa
willian156 <willian1561@outlook.com>
2020-11-17 18:31:31 -0300

    RODAR O ARQUIVO 'emocao.py'

---
+++ Programa funcionando/converttotext.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+def ouvir_microfone():
+  microfone = sr.Recognizer()
+  with sr.Microphone() as source:
+    microfone.adjust_for_ambient_noise(source)
+    print("Diga alguma coisa: ")
+    audio = microfone.listen(source)
+  try:
+    frase = microfone.recognize_google(audio,language='pt-BR')
+    print("Você disse: " + frase)
+  except sr.UnknownValueError:
+    print("Não entendi")
+  return frase
